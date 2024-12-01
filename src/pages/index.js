06570332df9cbfd8b85ef6d3334cb36b4045232f
Narain Mittal <mittalnarain123@gmail.com>
2024-12-01 19:19:25 +0530

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,8 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
-        <br />
+Hi There!!!        <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
       <p style={paragraphStyles}>
