9c6b958db884c225c2d576564e69627d4bc20f13
Narain Mittal <mittalnarain123@gmail.com>
2024-12-03 18:46:10 +0530

    Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
+import LandingPage from '../components/LandingPage';
 
 export default function HomePage() {
   return (
-    <Layout>
-      <Hero />
-      <Services />
-    </Layout>
+    <LandingPage/>
   );
 }
