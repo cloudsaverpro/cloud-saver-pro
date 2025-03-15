9e03ac1bfef8fb7c5cd6e649482266fb7548422d
Narain Mittal <mittalnarain123@gmail.com>
2025-03-15 11:03:08 +0530

    Update SEO tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
   siteMetadata: {
     title: 'Cloud Cost Optimization Services',
     description: 'Professional cloud cost optimization consulting',
-    siteUrl: `https://www.cloudsaverpro.com`,
-    image: `src/images/csp.png`,
+    siteUrl: `https://www.cloudsaverpro.com/`,
+    image: `csp.png`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
